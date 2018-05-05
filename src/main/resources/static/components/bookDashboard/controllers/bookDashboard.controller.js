2f716c205f2bbf0c5c673a0f76df343d6c663912
DESKTOP-D64SFO8\Coteanu-Sebastian <sebastiancoteanu@gmail.com>
2018-05-05 16:16:21 +0300

    Finishes filters; Relates chestia aia

--- src/main/resources/static/components/bookDashboard/controllers/bookDashboard.controller.js
+++ src/main/resources/static/components/bookDashboard/controllers/bookDashboard.controller.js
@@ -49,8 +49,22 @@
             {id: 3,description: 'Price High to Low'}
         ];
 
+        vm.categories = [
+            {id: 0,description: 'Adventure', isSelected: false},
+            {id: 1,description: 'Cooking', isSelected: false},
+            {id: 2,description: 'Crime', isSelected: false},
+            {id: 3,description: 'Educational', isSelected: false},
+            {id: 4,description: 'History', isSelected: false},
+            {id: 5,description: 'Mysteries', isSelected: false},
+            {id: 6,description: 'Religion', isSelected: false},
+            {id: 7,description: 'Romance', isSelected: false},
+            {id: 8,description: 'Sports', isSelected: false},
+            {id: 9,description: 'Travel', isSelected: false}
+        ];
+
         vm.typeheadOnSelect = typeheadOnSelect;
         vm.sortBooks = sortBooks;
+        vm.toggleCheckbox = toggleCheckbox;
 
         //////////
 
@@ -62,6 +76,11 @@
 
         function sortBooks() {
             console.log(vm.orderSelection);
+        }
+
+        function toggleCheckbox(element) {
+            element.isSelected = !element.isSelected;
+            console.log(vm.categories);
         }
 
         function _initCtrl() {
