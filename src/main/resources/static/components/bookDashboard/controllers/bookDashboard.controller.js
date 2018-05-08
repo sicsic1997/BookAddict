54ef83e737b24eaf1474d97c81d1faf658400752
DESKTOP-D64SFO8\Coteanu-Sebastian <sebastiancoteanu@gmail.com>
2018-05-09 02:22:34 +0300

    Finish adding components. Start integration filters with results from [BE].

--- src/main/resources/static/components/bookDashboard/controllers/bookDashboard.controller.js
+++ src/main/resources/static/components/bookDashboard/controllers/bookDashboard.controller.js
@@ -53,18 +53,40 @@
             {id: 0,description: 'Adventure', isSelected: false},
             {id: 1,description: 'Cooking', isSelected: false},
             {id: 2,description: 'Crime', isSelected: false},
+            {id: 11,description: 'Drama', isSelected: false},
             {id: 3,description: 'Educational', isSelected: false},
+            {id: 10,description: 'Fantasy', isSelected: false},
             {id: 4,description: 'History', isSelected: false},
-            {id: 5,description: 'Mysteries', isSelected: false},
+            {id: 5,description: 'Mystery', isSelected: false},
             {id: 6,description: 'Religion', isSelected: false},
             {id: 7,description: 'Romance', isSelected: false},
             {id: 8,description: 'Sports', isSelected: false},
+            {id: 9,description: 'Thriller', isSelected: false},
             {id: 9,description: 'Travel', isSelected: false}
+        ];
+
+        vm.books = [
+            {id: 1, category: ['Adventure', 'Fantasy'], author: 'Paulo Coelho', title: 'The Alchemist', isSelected: false, backgroundUrl: ""},
+            {id: 2, category: ['Crime', 'Drama', 'Mystery'], author: 'Agatha Christie', title: 'Murder on the Orient Express', isSelected: false, backgroundUrl: ""},
+            {id: 3, category: ['Adventure', 'Fantasy'], author: 'J. R. R. Tolkien', title: 'The Lord of the Rings', isSelected: false, backgroundUrl: ""},
+            {id: 4, category: ['Crime', 'Mystery'], author: 'Michael Connelly', title: 'The Lincoln Lawyer', isSelected: false, backgroundUrl: ""},
+            {id: 5, category: ['Fantasy', 'Thriller'], author: 'Joakim Zander', title: 'The Brother', isSelected: false, backgroundUrl: ""},
+            {id: 6, category: ['Adventure', 'Fantasy'], author: 'Paulo Coelho', title: 'The Alchemist', isSelected: false, backgroundUrl: ""},
+            {id: 7, category: ['Crime', 'Drama', 'Mystery'], author: 'Agatha Christie', title: 'Murder on the Orient Express', isSelected: false, backgroundUrl: ""},
+            {id: 8, category: ['Adventure', 'Fantasy'], author: 'J. R. R. Tolkien', title: 'The Lord of the Rings', isSelected: false, backgroundUrl: ""},
+            {id: 9, category: ['Crime', 'Mystery'], author: 'Michael Connelly', title: 'The Lincoln Lawyer', isSelected: false, backgroundUrl: ""},
+            {id: 10, category: ['Fantasy', 'Thriller'], author: 'Joakim Zander', title: 'The Brother', isSelected: false, backgroundUrl: ""},
+            {id: 11, category: ['Adventure', 'Fantasy'], author: 'Paulo Coelho', title: 'The Alchemist', isSelected: false, backgroundUrl: ""},
+            {id: 12, category: ['Crime', 'Drama', 'Mystery'], author: 'Agatha Christie', title: 'Murder on the Orient Express', isSelected: false, backgroundUrl: ""},
+            {id: 13, category: ['Adventure', 'Fantasy'], author: 'J. R. R. Tolkien', title: 'The Lord of the Rings', isSelected: false, backgroundUrl: ""},
+            {id: 14, category: ['Crime', 'Mystery'], author: 'Michael Connelly', title: 'The Lincoln Lawyer', isSelected: false, backgroundUrl: ""},
+            {id: 15, category: ['Fantasy', 'Thriller'], author: 'Joakim Zander', title: 'The Brother', isSelected: false, backgroundUrl: ""}
         ];
 
         vm.typeheadOnSelect = typeheadOnSelect;
         vm.sortBooks = sortBooks;
         vm.toggleCheckbox = toggleCheckbox;
+        vm.toggleBookSelection = toggleBookSelection;
 
         //////////
 
@@ -85,6 +107,11 @@
 
         function _initCtrl() {
             vm.orderSelection = vm.orderOptions[0];
+        }
+
+        function toggleBookSelection(book) {
+            book.isSelected = !book.isSelected;
+            console.log(vm.books);
         }
 
         $scope.state = 'CA';
